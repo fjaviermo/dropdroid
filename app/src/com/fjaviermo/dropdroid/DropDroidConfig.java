8756727cdab793ffcc836de0290a10f0564d7fbd
Francisco Javier Martin Otero <fjavier.mo@gmail.com>
2013-12-12 16:42:49 +0100

    Added dropbox config

---
+++ app/src/com/fjaviermo/dropdroid/DropDroidConfig.java
@@ -0,0 +1,18 @@
+package com.fjaviermo.dropdroid;
+
+import android.content.Context;
+
+import com.dropbox.sync.android.DbxAccountManager;
+
+public class DropDroidConfig {
+
+    private DropDroidConfig() {}
+
+    public static final String APP_KEY = "tre5u0zdqv0wnv8";
+    public static final String APP_SECRET = "ytmc69de98qhplx";
+
+    public static DbxAccountManager getAccountManager(Context context)
+    {
+        return DbxAccountManager.getInstance(context.getApplicationContext(), APP_KEY, APP_SECRET);
+    }
+}
